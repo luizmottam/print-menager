e708869ea5051d32b97fed6341f68024b328d7dd
Luiz Motta <luizdamm@gmail.com>
2024-07-15 23:38:01 -0300

    Adicionando tabela a tela Conts

--- printhub/src/pages/Conts.jsx
+++ printhub/src/pages/Conts.jsx
@@ -1,7 +1,33 @@
+import "../style/Conts.css";
+
 export default function Conts() {
   return (
     <>
     <h1>Cont. de Papel</h1>
+
+    <table>
+        <thead>
+          <tr>
+            <th>Nome</th>
+            <th>Preto</th>
+            <th>Colorido</th>
+            <th>Magenta</th>
+            <th>Ciano</th>
+            <th>Amarelo</th>
+          </tr>
+        </thead>
+        <tbody>
+          <td>IM-11</td>
+          <td>900</td>
+          <td>810</td>
+          <td>340</td>
+          <td>217</td>
+          <td>253</td>
+        </tbody>
+        <tbody>
+          
+        </tbody>
+      </table>
     </>
   )
 }
